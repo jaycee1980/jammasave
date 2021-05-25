c87aa32c811dd243ad595e34b98bd77738fd2ff8
Peter Mulholland <peter.mulholland@vpltd.com>
2021-05-25 18:40:24 +0100

    Optimised and commented code

--- firmware/jammasave.c
+++ firmware/jammasave.c
@@ -27,17 +27,17 @@ FUSES = {
 //----------------------------------------------------------------------------------------------------------------------
 // Constants and Defines
 
-// The timer ticks at 750KHz. A count of 150 means our ISR triggers every 5KHz
+// The timer ticks at 75KHz. A count of 150 means our ISR triggers every 5KHz
 #define TIMER_COUNT		(256-150)
 
 // So, 500 ticks = 1 second.
 // In reality it's a little inaccurate because the 4.8MHz internal clock isn't that accurate.
-#define TIMER_TICKS		500
+#define TIMER_TICKS_PER_SEC		500
 
 //----------------------------------------------------------------------------------------------------------------------
 // PROGMEM data
 
-static const char c_credits[] __attribute__((used)) PROGMEM = "CCH JAMMA Power Saver by Peter =M= built " __DATE__ " " __TIME__;
+static const char c_credits[] __attribute__((used)) PROGMEM = "CCH JAMMA Power Saver built " __DATE__ " " __TIME__;
 
 //----------------------------------------------------------------------------------------------------------------------
 // EEMEM data
@@ -51,34 +51,6 @@ uint32_t g_timeout = 0;
 //----------------------------------------------------------------------------------------------------------------------
 // Local Functions
 
-void inline stop_timer()
-{
-	// Disable overflow timer
-	TIMSK0 &= ~(1 << TOIE0);								// overflow interrupt off
-	TIFR0  &= ~(1 << TOV0);									// clear timer overflow mask
-	TCCR0A = 0;
-	TCCR0B = 0;
-	TCNT0 = 0;
-}
-
-void start_timer()
-{
-	cli();
-
-	// Stop timer in case it's running already
-	stop_timer();
-
-	// Reset our timer counter
-	g_timer = g_timeout;
-
-	// Enable overflow timer
-	TCNT0 = TIMER_COUNT;
-	TIMSK0 = (1 << TOIE0);									// overflow interrupt on
-	TCCR0B = (1 << CS01) | (1 << CS00);						// 4.8MHz / 64 = ticks at 75KHz
-
-	sei();
-}
-
 uint16_t read_adc()
 {
 	ADCSRA |= (1 << ADSC);									// Start conversion
@@ -91,7 +63,7 @@ uint16_t read_adc()
 void read_timeout_setting()
 {
 	// Set up ADC with ADC1 (PB2) as the input
-	DIDR0 = (1 << ADC1D);									// Disable digital IO on ADC1
+	DIDR0 = (1 << ADC1D);									// Disable digital IO on PB2
 	ADMUX = (1 << MUX1);									// Use Vcc as reference, right adjusted result, input from ADC1 (PB2)
 	ADCSRB = 0;
 	ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS0);		// Enable ADC, no interrupt, CLK/32 (150KHz)
@@ -108,7 +80,7 @@ void read_timeout_setting()
 	// Set timer to the timeout count - between 1/2 hour and 1 1/2 hour.
 	//	g_timeout =  1800 + ((((3600 * 32) / 512) * (uint32_t) tcnt) / 32);
 	g_timeout  = ((225UL * (uint32_t) tcnt) / 32UL ) + 1800UL;
-	g_timeout *= (uint32_t) TIMER_TICKS;
+	g_timeout *= (uint32_t) TIMER_TICKS_PER_SEC;
 
 	// We don't use the ADC any more, so turn it off
 	ADCSRA = 0;
@@ -120,16 +92,16 @@ void read_timeout_setting()
 
 ISR(TIM0_OVF_vect)
 {
-	// Tick tock (for debugging, so i can see the timer running on ze 'scope)
-	PINB = (1 << PINB5);
-
 	// If we go over the timeout count, turn the relay off and stop the timer
 	// We then wait for a pin change interrupt to wake things up again
 
 	if (--g_timer == 0)
 	{
-		stop_timer();
+		// Turn relay off
 		PORTB &= ~(1 << PORTB4);
+
+		// Stop the timer
+		TCCR0B = 0;
 	}
 	else
 	{
@@ -141,11 +113,25 @@ ISR(TIM0_OVF_vect)
 
 ISR(PCINT0_vect)
 {
-	// Reset the timer
-	start_timer();
+	// If our pin change was low, the user pressed a control. Stop timer, and turn the relay on if it isn't.
+	// if our pin change was high, reset and start the timer again.
+	// This behaviour means that a stuck-on input will only result in the timeout never happening...
+	if ((PINB & (1 << PINB3)) == 0)
+	{
+		// Turn the relay on if it isn't already
+		PORTB |= (1 << PORTB4);
 
-	// Turn the relay on if it isn't already
-	PORTB |= (1 << PORTB4);
+		// Stop the timer
+		TCCR0B = 0;
+	}
+	else
+	{
+		// Reset and start the timer
+		g_timer = g_timeout;
+
+		TCNT0 = TIMER_COUNT;
+		TCCR0B = (1 << CS01) | (1 << CS00);
+	}
 }
 
 //----------------------------------------------------------------------------------------------------------------------
@@ -155,31 +141,26 @@ void main(void) __attribute__((noreturn));
 
 void main(void)
 {
-	// Tell the watchdog to bugger off
+	// Disable the watchdog timer
 	wdt_disable();
 
 	// Enable pullup on PB1, set output ports low
 	PORTB = (1 << PORTB1);
 
-	// Set all pins as output except PB1-3 and which are inputs
-	DDRB = (1 << DDB4) | (1 << DDB5);
+	// Set PB4 as output (relay control), the rest to inputs
+	DDRB = (1 << DDB4);
 
 	// Wait for port state to synchronise before reading it
 	_NOP();
-	_NOP();	// two are needed even though atmel says only one!
+	_NOP();
 
 	// Check if the "disable" jumper connected to PB1 is present (PB1 will be pulled low)
 	// If it is, we don't do anything else - just turn on the relay and go to sleep permanently
 
 	if (!(PINB & (1 << PINB1)))
 	{
-		// Hmm, why two separate ops to PORTB ? GCC generates two SBI's rather than an IN/ORI/OUT this way!
-
 		// Turn relay on
 		PORTB |= (1 << PORTB4);
-
-		// Set PB5 high to show we've entered "disable" mode
-		PORTB |= (1 << PORTB5);
 
 		// We never wake up again...
 		set_sleep_mode(SLEEP_MODE_PWR_DOWN);
@@ -197,8 +178,18 @@ void main(void)
 		// Turn relay on
 		PORTB |= (1 << PORTB4);
 
-		// Start timer (which enables interrupts)
-		start_timer();
+		// Set timeout counter
+		g_timer = g_timeout;
+
+		// Set up the overflow timer. 4.8MHz / 64 = ticks at 75KHz.
+		TIMSK0 = (1 << TOIE0);					// TIMER0_OVF interrupt on
+		TIFR0 = 0;								// Clear timer flags
+		TCCR0A = 0;								// No compare match output, normal waveform mode
+
+		TCNT0 = TIMER_COUNT;
+		TCCR0B = (1 << CS01) | (1 << CS00);		// Set divider and start
+
+		sei();									// Enable interrupts so that timer overflow and pin change wake us up
 
 		// Set our sleep mode so timer and pin change interrupts work
 		set_sleep_mode(SLEEP_MODE_IDLE);
